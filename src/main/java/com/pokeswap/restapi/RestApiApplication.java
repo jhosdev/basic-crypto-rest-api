612f717a527dfaf6fd5d6c19134416ab792e66a1
Jhosaim <86936147+jhosR@users.noreply.github.com>
2023-06-10 02:55:09 -0500

    bd cors fix

--- src/main/java/com/pokeswap/restapi/RestApiApplication.java
+++ src/main/java/com/pokeswap/restapi/RestApiApplication.java
@@ -2,6 +2,11 @@ package com.pokeswap.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class RestApiApplication {
@@ -10,4 +15,25 @@ public class RestApiApplication {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+
+		// Allow all origins access
+		//config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		// Register CORS configuration
+		source.registerCorsConfiguration("/**", config);
+
+		registrationBean.setFilter(new CorsFilter(source));
+		registrationBean.setOrder(0);
+
+		// Return the CORS filter
+		return registrationBean;
+	}
 }
